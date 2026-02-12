268df12c0b17fb4a5a5f01b00af7ae2c6afda871
ermias-tesfaye <ermiastesfaye28@gmail.com>
2026-02-12 16:40:36 +0300

    delete + update

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,6 +1,7 @@
 using asp.net_crud.Context;
 using asp.net_crud.Models;
 using asp.net_crud.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,6 +18,7 @@ namespace asp.net_crud.Controllers
         {
             this.dbContext = dbContext;
         }
+
         [HttpGet]
         public IActionResult GetAllEmployees()
         {
@@ -40,8 +42,58 @@ namespace asp.net_crud.Controllers
             dbContext.Employees.Add(employeeEntity);
             dbContext.SaveChanges();
 
-            return StatusCode(201,employeeEntity);
+            return StatusCode(201, employeeEntity);
 
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetAllEmployeeById(Guid id)
+        {
+            var EmployeeData = dbContext.Employees.Find(id);
+
+            if (EmployeeData == null)
+                return NotFound();
+
+            return Ok(EmployeeData);
+
+        }
+
+        [HttpPut]
+
+        [Route("{id:guid}")]
+        public IActionResult UpdateEmployee(Guid id, UpdateEmployeeDto updateEmployeeDto)
+        {
+
+            var employee = dbContext.Employees.Find(id);
+
+            if (employee == null)
+                return NotFound();
+
+
+            employee.Name = updateEmployeeDto.Name;
+            employee.Email = updateEmployeeDto.Email;
+            employee.Phone = updateEmployeeDto.Phone;
+            employee.Salary = updateEmployeeDto.Salary;
+
+
+            dbContext.SaveChanges();
+
+            return Ok(employee);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+            if (employee == null)
+                return NotFound();
+
+            dbContext.Employees.Remove(employee);
+            dbContext.SaveChanges();
+
+            return NoContent();
         }
     }
 }
---
+++ Models/UpdateEmployeeDto.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace asp.net_crud.Models;
+
+public class UpdateEmployeeDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+
+    public required string Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public decimal Salary { get; set; }
+}
